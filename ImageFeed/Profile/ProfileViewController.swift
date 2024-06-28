7b28083d05699c7299b2f71b43dc42c98b3d1545
Aleksei Sidenko <sidoi90@bk.ru>
2024-06-28 17:45:45 +0500

    Profile screen layout

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Алексей Сиденко on 28.06.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
